00eb10ede73d4c67a53f9fc9a08b4a69df325c4f
ishrathjahaan0786 <ishrathjahaan0786@gmail.com>
2026-01-22 17:27:49 +0530

    Add arithmetic operations and print results

---
+++ Arithmetic_operations and  concatenation.py
@@ -0,0 +1,14 @@
+a = 20
+b = 10
+
+addition = a + b
+print("Addition:", addition)
+
+subtraction = a - b
+print("Subtraction:", subtraction)
+
+multiplication = a * b
+print("Multiplication:", multiplication)
+
+division = a / b
+print("Division:", division)
