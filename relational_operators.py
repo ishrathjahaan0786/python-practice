0987946aa30ce263e8d18398f1b3c6b7f0e656a3
ishrathjahaan0786 <ishrathjahaan0786@gmail.com>
2026-01-23 14:34:35 +0530

    Add demonstration of relational operators
    Demonstrate usage of relational operators in Python.

---
+++ relational_operators.py
@@ -0,0 +1,13 @@
+# some relational operators are used in boolean data types
+# Program to demonstrate relational operators in Python
+# Relational operators return boolean values (True or False)
+
+a = 15
+b = 10
+
+print("a == b :", a == b)   # Equal to
+print("a != b :", a != b)   # Not equal to
+print("a > b  :", a > b)    # Greater than
+print("a < b  :", a < b)    # Less than
+print("a >= b :", a >= b)   # Greater than or equal to
+print("a <= b :", a <= b)   # Less than or equal to
