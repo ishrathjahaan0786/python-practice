50861aaf6d6c0855edd6d9cf8912766550afed0d
ishrathjahaan0786 <ishrathjahaan0786@gmail.com>
2026-01-23 14:41:01 +0530

    Add conversion of number to float

--- type_conversions.py
+++ type_conversions.py
@@ -8,7 +8,9 @@ height_int = int(height)
 
 number = 10
 number_str = str(number)
+num = float(number)
 
 print("Age:", age)
 print("Height (int):", height_int)
 print("Number as string:", number_str)
+print("Number as float:", num)
